388dca4e3fcf0b530d02b692ddaf237867b5fcde
Vaishak K <vaishak610@gmail.com>
2021-09-24 11:12:35 +0000

    UI fixes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function validations(field,regex,className,fieldType){
     let fieldName = field.value;
     let fieldRegex = regex;
     if(fieldRegex.test(fieldName)){
-        document.querySelector(className).innerHTML = `Valid ${fieldType}: <b class='valid-field'>${fieldName}</b>`;
+        document.querySelector(className).innerHTML = `Valid ${fieldType}: <strong class='valid-field'>${fieldName}</strong>`;
     } else if(fieldName === ""){
         document.querySelector(className).innerHTML = "" ;
     } else {
-        document.querySelector(className).innerHTML = `<b class='invalid-field'>Invalid ${fieldType}</b>`;
+        document.querySelector(className).innerHTML = `<strong class='invalid-field'>Invalid ${fieldType}</strong>`;
     }
 }
